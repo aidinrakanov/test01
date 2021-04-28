fc7a5b1e5578a895e314d632543e261bce028e67
aidinrakanov <altynaikanatbekova@gmail.com>
2021-04-29 02:53:37 +0600

    4

--- app/src/main/java/com/example/sanatorii/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/sanatorii/ui/main/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.sanatorii.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.example.sanatorii.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +23,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setupNavigation()
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.navigation_home ||
+                destination.id == R.id.navigation_posts ||
+                destination.id == R.id.navigation_profile
+            ) {
+                nav_view.visibility = View.VISIBLE
+            } else {
+                nav_view.visibility = View.GONE
+            }
+        }
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
